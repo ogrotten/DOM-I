1254eba894d55759a3dd133681834af23d80cbaa
Darrin Lowery <lambda@monsto.com>
2019-09-30 17:03:35 -0500

    main-content > features and about

--- js/index.js
+++ js/index.js
@@ -48,21 +48,58 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 function clg (x){console.log(x);}
 
-// nav
+// nav -------------------------------
 const nav = Array.from(document.querySelectorAll('nav a'));
 
 const navNew = Object.entries(siteContent.nav);
-  // I realize that `object.entries` doesn't guarantee order,
-  // but it works in this assignment.
+// I realize that `object.entries` doesn't guarantee order,
+// but it works in this assignment.
 
 for (let i = 0; i < nav.length; i++) {
   const e = nav[i];
   e.setAttribute("class", navNew[i][0]);
   e.textContent = navNew[i][1];
   // clg(navNew[i]);
+  
   // clg(e);
 }
 
+// section.cta -------------------------------
 document.getElementById("cta-img").src= siteContent.cta["img-src"];
 document.querySelector(".cta h1").textContent = siteContent.cta.h1;
 document.querySelector(".cta button").textContent = siteContent.cta.button;
+
+// section.main-content -------------------------------
+
+const main_top_text = Array.from(document.querySelectorAll('.top-content .text-content'));
+const main_bottom = Array.from(document.querySelectorAll('.bottom-content .text-content'));
+const mainNewData = Object.entries(siteContent["main-content"]);
+
+let contCount = 0;
+let topContOut = "";
+
+
+for (let i = 0; i < document.querySelectorAll('.top-content .text-content').length; i++) {
+  document.querySelector('.top-content').children[i].children[0].textContent = mainNewData[contCount][1]
+  contCount++
+  document.querySelector('.top-content').children[i].children[1].textContent = mainNewData[contCount][1]
+  contCount++
+}
+
+
+
+
+// // clg(main_top_text[0])
+// clg(mainNewData)
+// // clg(Array.from(document.getElementsByClassName("text-content")).length)
+
+// for (let i = 0; i < document.getElementsByClassName("text-content").length; i++) {
+//   const e = document.getElementsByClassName("text-content")[i];
+//   // e.querySelector("h4").textContent = mainNewData[i][0];
+//   clg(mainNewData[i][0]);
+// }
+
+
+// // Array.from(document.getElementsByClassName("text-content")).forEach(e => {
+// //   let out = e
+// // });
